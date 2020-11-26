040b1e61e0d181ec84f9196ffc352d037a4df160
Edcka <edvardastamb@gmail.com>
2020-11-27 01:01:39 +0200

    Commit 1.1

--- app/src/main/java/com/example/activitiesandstorage/MainActivity.java
+++ app/src/main/java/com/example/activitiesandstorage/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.activitiesandstorage;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button buttonAdd, buttonDelete;
+    RecyclerView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
